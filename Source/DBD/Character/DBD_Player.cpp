269f487e22739d09beeb68198ea45f73374bfa1b
VecTor <lsh961101@naver.com>
2024-11-18 19:11:14 +0900

    PlayerAnimation ver0.1

--- Source/DBD/Character/DBD_Player.cpp
+++ Source/DBD/Character/DBD_Player.cpp
@@ -3,3 +3,135 @@
 
 #include "DBD_Player.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+#include "EnhancedInputComponent.h"
+
+void ADBD_Player::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Health = MaxHealth;
+}
+
+void ADBD_Player::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ADBD_Player::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(PlusHpAction, ETriggerEvent::Started, this, &ADBD_Player::PlusHp);
+		EnhancedInputComponent->BindAction(MinusHpAction, ETriggerEvent::Started, this, &ADBD_Player::MinusHp);
+
+		EnhancedInputComponent->BindAction(RunAction, ETriggerEvent::Triggered, this, &ADBD_Player::Run);
+		EnhancedInputComponent->BindAction(RunAction, ETriggerEvent::Completed, this, &ADBD_Player::RunStop);
+
+		EnhancedInputComponent->BindAction(CrouchAction, ETriggerEvent::Triggered, this, &ADBD_Player::Crouch);
+		EnhancedInputComponent->BindAction(CrouchAction, ETriggerEvent::Completed, this, &ADBD_Player::CrouchStop);
+	}
+}
+
+void ADBD_Player::PlusHp()
+{
+	UpdateHP(-1);
+}
+
+void ADBD_Player::MinusHp()
+{
+	UpdateHP(1);
+}
+
+void ADBD_Player::Run()
+{
+	// Health가 1이면 리턴
+	if (Health == 1) return;
+
+	IsRunning = true;
+	GetCharacterMovement()->MaxWalkSpeed = 400.0f;
+}
+
+void ADBD_Player::RunStop()
+{
+	// Health가 1이면 리턴
+	if (Health == 1) return;
+
+	IsRunning = false;
+	GetCharacterMovement()->MaxWalkSpeed = 226.0f;
+}
+
+void ADBD_Player::Crouch()
+{
+	// Health가 3일때만 작동
+	if (Health != 3) return;
+
+	IsCrouching = true;
+	GetCharacterMovement()->MaxWalkSpeed = 113.0f;
+}
+
+void ADBD_Player::CrouchStop()
+{
+	// Health가 3일때만 작동
+	if (Health != 3) return;
+
+	IsCrouching = false;
+	GetCharacterMovement()->MaxWalkSpeed = 226.0f;
+}
+
+void ADBD_Player::UpdateHP(int32 Value)
+{
+	// Value : Damage or Heal
+	Health -= Value;
+
+	if (Health > MaxHealth)
+	{
+		Health = MaxHealth;
+	}
+	else if (Health < 0)
+	{
+		Health = 0;
+	}
+
+
+	// 3 : WalkSpeed 226, RunSpeed : 400
+	// crouch : WalkSpeed 113
+	// 2 : WalkSpeed 226, RunSpeed : 400
+	// 1 : WalkSpeed 70
+	// 피격시 2초 스피드 : 600
+
+	if (Health == 3)
+	{
+		GetCharacterMovement()->MaxWalkSpeed = 226.0f;
+	}
+	else if (Health == 2)
+	{
+		GetCharacterMovement()->MaxWalkSpeed = 226.0f;
+	}
+	else if (Health == 1)
+	{
+		GetCharacterMovement()->MaxWalkSpeed = 70.0f;
+	}
+
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Health : %d"), Health));
+}
+
+void ADBD_Player::UpdateSpeed()
+{
+	switch (Health)
+	{
+	case 3:
+		GetCharacterMovement()->MaxWalkSpeed = 226.0f;
+		break;
+	case 2:
+		GetCharacterMovement()->MaxWalkSpeed = 226.0f;
+		break;
+	case 1:
+		GetCharacterMovement()->MaxWalkSpeed = 70.0f;
+		break;
+	default:
+		break;
+	}
+}
--- Source/DBD/Character/DBD_Player.h
+++ Source/DBD/Character/DBD_Player.h
@@ -14,4 +14,52 @@ class DBD_API ADBD_Player : public ADBDCharacter
 {
 	GENERATED_BODY()
 	
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+	void PlusHp();
+	void MinusHp();
+	void Run();
+	void RunStop();
+	void Crouch();
+	void CrouchStop();
+public:
+	// 생존자의 HP 업데이트 함수
+	void UpdateHP(int32 Damage);
+	void UpdateSpeed();
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	inline int32 GetHealth() const {return Health;}
+	inline bool GetIsRunning() const {return IsRunning;}
+	inline bool GetCrouching() const {return IsCrouching;}
+
+private:
+	// 임시용 체력 변경 키값
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* PlusHpAction;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* MinusHpAction;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* RunAction;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* CrouchAction;
+
+
+
+	// 생존자 체력
+	UPROPERTY(EditAnywhere, Category = "Survivor")
+	int32 Health; // 3 : 멀쩡 2 : 부상 1 : 중상 0 : 죽음
+
+	UPROPERTY(EditAnywhere, Category = "Survivor")
+	int32 MaxHealth = 3; 
+
+	// 뛰는지 안뛰는지 체크 변수
+	bool IsRunning = false;
+	// Crouch 상태인지 체크 변수
+	bool IsCrouching = false;
 };
