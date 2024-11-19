b2be55ea8ddfdb85caf8ed015520e5045fe6021a
jaemin-han <duddjakgkrl@gmail.com>
2024-11-19 17:58:56 +0900

    [feat] killer 의 공격이 한 번만 발생하게 수정

--- Source/DBD/Animation/ANS_Attack.cpp
+++ Source/DBD/Animation/ANS_Attack.cpp
@@ -3,12 +3,27 @@
 
 #include "ANS_Attack.h"
 
+#include "Character/DBD_Player.h"
+
 void UANS_Attack::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime,
                              const FAnimNotifyEventReference& EventReference)
 {
+	// 이미 공격이 발생했었으면 더 이상 공격을 처리하지 않음
+	if (bHasValidHitOccurred)
+		return;
+	
 	Super::NotifyTick(MeshComp, Animation, FrameDeltaTime, EventReference);
 
 	PerformSphereTrace(MeshComp);	
+}
+
+void UANS_Attack::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+	const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyEnd(MeshComp, Animation, EventReference);
+
+	// 공격이 끝났을 때, 다음 공격을 위해 변수를 초기화
+	bHasValidHitOccurred = false;
 }
 
 FHitResult UANS_Attack::PerformSphereTrace(USkeletalMeshComponent* MeshComp)
@@ -25,12 +40,12 @@ FHitResult UANS_Attack::PerformSphereTrace(USkeletalMeshComponent* MeshComp)
 	FHitResult HitResult;
 	float SphereRadius = 50.0f;
 
-	bool bHit = MeshComp->GetWorld()->SweepSingleByObjectType(
+	bool bHit = MeshComp->GetWorld()->SweepSingleByProfile(
 		HitResult,
 		StartLocation,
 		EndLocation,
 		FQuat::Identity,
-		FCollisionObjectQueryParams(ECC_Pawn),
+		FName("KillerWeapon"),
 		FCollisionShape::MakeSphere(SphereRadius),
 		Params
 		);
@@ -39,6 +54,15 @@ FHitResult UANS_Attack::PerformSphereTrace(USkeletalMeshComponent* MeshComp)
 	{
 		UE_LOG(LogTemp, Log, TEXT("Sphere Trace hit: %s"), *HitResult.GetActor()->GetName());
 
+		// HitResult 의 결과가 ADBD_Player 인 경우, 플레이어에게 데미지를 입히는 로직을 추가
+		auto* Survivor =Cast <ADBD_Player>(HitResult.GetActor());
+		if (Survivor)
+		{
+			// Todo: 플레이어에게 데미지를 입히는 로직 추가, 승호님과 상의 필요
+			Survivor->UpdateHP(1);
+			// 중복 공격을 막기 위해 유효한 공격이 발생했음을 표시
+			bHasValidHitOccurred = true;
+		}
 		// Optional: Debug visualization
 		DrawDebugSphere(MeshComp->GetWorld(), HitResult.ImpactPoint, 2.0f, 12, FColor::Green, false, 0.5f);
 	}
--- Source/DBD/Animation/ANS_Attack.h
+++ Source/DBD/Animation/ANS_Attack.h
@@ -16,10 +16,14 @@ class DBD_API UANS_Attack : public UAnimNotifyState
 
 public:
 	virtual void NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference) override;
+	virtual void NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
 
 	FName SocketStart = "WeaponSocket_l";
 	FName SocketEnd = "WeaponSocket_l_End";
 
 protected:
 	FHitResult PerformSphereTrace(USkeletalMeshComponent* MeshComp);
+
+	// 중복 공격을 막기 위해 유효한 공격이 발생했었는지를 나타내는 변수
+	bool bHasValidHitOccurred = false;
 };
