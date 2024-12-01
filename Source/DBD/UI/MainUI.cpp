59aa6bbaf75612b479109444b25c451ce0ec6a5f
jaemin-han <duddjakgkrl@gmail.com>
2024-12-02 02:08:54 +0900

    [feat] 세션 구현
    - PlayLevel 에서 PlayGameMode 를 사용하게 구현, 서버 꺼는 킬러, 클라 꺼는 생존자 스폰하게 구현함
    - SeqLevel - Level Blueprint 에서 설정한 InputModeUIOnly 를 풀기ADBDCharacter-BeginPlay 에서 InputModeGameOnly 를 설정해 줌

--- Source/DBD/UI/MainUI.cpp
+++ Source/DBD/UI/MainUI.cpp
@@ -3,10 +3,12 @@
 
 #include "MainUI.h"
 
-#include "Character/Killer.h"
+#include "SessionBlock.h"
 #include "Components/Button.h"
-#include "GameMode/DBDGameMode.h"
-#include "Kismet/GameplayStatics.h"
+#include "Components/EditableTextBox.h"
+#include "Components/ScrollBox.h"
+#include "Components/WidgetSwitcher.h"
+#include "GameMode/DBDGameInstance.h"
 
 class ADBDGameMode;
 
@@ -14,16 +16,60 @@ void UMainUI::NativeConstruct()
 {
 	Super::NativeConstruct();
 
+	GameInstance = Cast<UDBDGameInstance>(GetGameInstance());
+	// bind delegate for adding session
+	GameInstance->OnAddSession.BindUObject(this, &UMainUI::OnAddSession);
+
 	// Bind the buttons
-	GamePlay_Btn->OnClicked.AddDynamic(this, &UMainUI::OnGamePlayBtnClicked);
+
+	// MainMenu
+	Btn_Killer->OnClicked.AddDynamic(this, &UMainUI::OnKillerClicked);
+	Btn_Survivor->OnClicked.AddDynamic(this, &UMainUI::OnSurvivorClicked);
+
+	// KillerMenu
+	Btn_CreateLobby->OnClicked.AddDynamic(this, &UMainUI::OnCreateLobbyClicked);
+	Btn_Back->OnClicked.AddDynamic(this, &UMainUI::OnBackClicked);
+
+	// SurvivorMenu
+	Btn_Search->OnClicked.AddDynamic(this, &UMainUI::OnSearchLobbyClicked);
+	Btn_Back2->OnClicked.AddDynamic(this, &UMainUI::OnBackClicked);
 }
 
-void UMainUI::OnGamePlayBtnClicked()
+void UMainUI::OnKillerClicked()
 {
-	// open level
-	UGameplayStatics::OpenLevel(GetWorld(), LevelName);
-	UGameplayStatics::GetPlayerController(GetWorld(),0)->SetInputMode(FInputModeGameOnly());
-	UGameplayStatics::GetPlayerController(GetWorld(),0)->bShowMouseCursor = false;
+	WidgetSwitcher->SetActiveWidgetIndex(1);
+}
+
+void UMainUI::OnSurvivorClicked()
+{
+	WidgetSwitcher->SetActiveWidgetIndex(2);
+}
+
+void UMainUI::OnCreateLobbyClicked()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Create Lobby Clicked"));
+	GameInstance->CreateSession(ETB_SessionName->GetText().ToString(), 5);
+}
+
+void UMainUI::OnBackClicked()
+{
+	WidgetSwitcher->SetActiveWidgetIndex(0);
+}
+
+void UMainUI::OnSearchLobbyClicked()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Search Lobby Clicked"));
+	SB_SessionList->ClearChildren();
+
+	GameInstance->FindOtherSessions();
+}
+
+void UMainUI::OnAddSession(int32 Index, FString Info)
+{
+	USessionBlock* SessionBlock = CreateWidget<USessionBlock>(this, SessionBlockClass);
+	SessionBlock->SetInfo(Index, Info);
+	SessionBlock->OnButtonClicked.BindUObject(GameInstance, &UDBDGameInstance::JoinOtherSession);
+	SB_SessionList->AddChild(SessionBlock);
 }
 
 
--- Source/DBD/UI/MainUI.h
+++ Source/DBD/UI/MainUI.h
@@ -15,15 +15,68 @@ class DBD_API UMainUI : public UUserWidget
 	GENERATED_BODY()
 
 	UPROPERTY(meta = (BindWidget))
-	class UButton* GamePlay_Btn;
+	class UWidgetSwitcher* WidgetSwitcher;
 
+protected:
 	virtual void NativeConstruct() override;
 
-	// game play button clicked
-	UFUNCTION()
-	void OnGamePlayBtnClicked();
-	
-
+public:
+	UPROPERTY()
+	class UDBDGameInstance* GameInstance;
 	// level name
 	FName LevelName{"SurvivorTestLevel"};
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<class USessionBlock> SessionBlockClass;
+
+#pragma region MainMenu
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	class UButton* Btn_Killer;
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* Btn_Survivor;
+
+	UFUNCTION()
+	void OnKillerClicked();
+	UFUNCTION()
+	void OnSurvivorClicked();
+
+#pragma endregion
+
+#pragma region KillerMenu
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	class UEditableTextBox* ETB_SessionName;
+	UPROPERTY(meta = (BindWidget))
+	class UButton* Btn_CreateLobby;
+	UPROPERTY(meta = (BindWidget))
+	class UButton* Btn_Back;
+
+private:
+	UFUNCTION()
+	void OnCreateLobbyClicked();
+	UFUNCTION()
+	void OnBackClicked();
+
+#pragma endregion
+
+#pragma region SurvivorMenu
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	class UScrollBox* SB_SessionList;
+	UPROPERTY(meta = (BindWidget))
+	class UButton* Btn_Search;
+	UPROPERTY(meta = (BindWidget))
+	class UButton* Btn_Back2;
+
+private:
+	UFUNCTION()
+	void OnSearchLobbyClicked();
+	UFUNCTION()
+	void OnAddSession(int32 Index, FString Info);
+	
+#pragma endregion
 };
