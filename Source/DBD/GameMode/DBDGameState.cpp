cf82ee234d7189e6127dfc905b7c146fad4f4532
jaemin-han <duddjakgkrl@gmail.com>
2024-12-03 21:01:31 +0900

    [feat] post process volume 생성

---
+++ Source/DBD/GameMode/DBDGameState.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DBDGameState.h"
+
+#include "GameFramework/Character.h"
+#include "GameFramework/PlayerState.h"
+
+ADBDGameState::ADBDGameState()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bStartWithTickEnabled = true;
+}
+
+void ADBDGameState::BeginPlay()
+{
+	Super::BeginPlay();
+
+	PostProcessVolume = GetWorld()->SpawnActor<APostProcessVolume>();
+	PostProcessVolume->bUnbound = true;
+	AddDynamicMaterialToPostProcessVolume();
+}
+
+
+void ADBDGameState::AddDynamicMaterialToPostProcessVolume()
+{
+	if (PostProcessVolume && MaterialInstance)
+	{
+		PostProcessVolume->AddOrUpdateBlendable(MaterialInstance);
+	}
+}
---
+++ Source/DBD/GameMode/DBDGameState.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "DBDGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DBD_API ADBDGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+
+	ADBDGameState();
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	class APostProcessVolume* PostProcessVolume;
+
+	UPROPERTY(EditDefaultsOnly)
+	class UMaterialInstance* MaterialInstance;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	void AddDynamicMaterialToPostProcessVolume();
+};
